bc57a7692272fe6a02c4050ab5a725ce4a288f96
emmax952 <emmax952@c-226-204.eduroam.liu.se>
2012-10-21 15:36:45 +0200

    Test getChannelIcon. Tested that it's not null

--- src/se/z_app/stb/api/zenterio/test/StandardCommandTest.java
+++ src/se/z_app/stb/api/zenterio/test/StandardCommandTest.java
@@ -2,6 +2,7 @@ package se.z_app.stb.api.zenterio.test;
 
 import java.util.Iterator;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 import se.z_app.stb.Channel;
 import se.z_app.stb.EPG;
@@ -12,22 +13,22 @@ import se.z_app.stb.api.zenterio.StandardCommand;
 import junit.framework.TestCase;
 
 public class StandardCommandTest extends TestCase {
-	
+
 	public void testGetCurrentChannel(){
 		StandardCommand cmd = new StandardCommand("130.236.248.226");
 		assertTrue(cmd.getCurrentChannel().getName() != null);
 		assertTrue(cmd.getCurrentChannel().getUrl() != null);
 	}
 	public void testGetEPG(){
-		
-		
+
+
 		StandardCommand cmd = new StandardCommand("130.236.248.226");
-		
+
 		long time = System.currentTimeMillis();
 		EPG epg = cmd.getEPG();
 		Log.i("ZmoteTestLog", "Total Featching and parsing EPG: " + (System.currentTimeMillis() - time) + "ms");
-		
-		
+
+
 		assertTrue(epg != null);
 
 		Iterator<Channel> epgIterator = epg.iterator();
@@ -56,33 +57,33 @@ public class StandardCommandTest extends TestCase {
 		}
 		Log.i("ZmoteTestLog", "Number of Channels: " + i);
 		assertTrue(i > 10);
-		
+
 		Channel channel = epg.getChannel(4);
 		assertTrue(channel.getNr() == 4);
-		
+
 		channel = epg.getChannel(10);
 		assertTrue(channel.getNr() == 10);
 	}
-	
+
 	public void testGetWebTVServices(){
 		StandardCommand cmd = new StandardCommand("130.236.248.226");
 		WebTVService sevices[] = cmd.getWebTVServices();
 		assertTrue(sevices.length == 3);
-		
+
 		for(int i = 0; i <3; i++){
 			assertTrue(sevices[i].getName() != null);
 			assertTrue(sevices[i].getID() != null);
 			assertTrue(sevices[i].getIconURL() != null);
 		}
-		
+
 	}
-	
+
 	public void testSearchWebTvService(){
 		StandardCommand cmd = new StandardCommand("130.236.248.226");
 		WebTVService sevices[] = cmd.getWebTVServices();
-		
+
 		assertTrue(sevices.length > 0);
-		
+
 		for(int i = 0; i < sevices.length; i++){
 			Log.i("SearchTest", "Sevice: " + sevices[i].getName() + ", num: " + i);
 			WebTVItem items[] = cmd.searchWebTVService("bill", sevices[i]);
@@ -96,8 +97,22 @@ public class StandardCommandTest extends TestCase {
 				assertTrue(items[j].getWebTVService() != null);
 			}
 		}
-			
+
+
+	}
+
+	public void testGetChannelImage(){
+		StandardCommand cmd = new StandardCommand("130.236.248.57");
+		Channel ch = new Channel();
+		Bitmap icon;
+		ch.setNr(1);
+		ch.setOnid(1);
+		ch.setSid(1000);
+		ch.setTsid(1);
 		
+		icon = cmd.getChannelIcon(ch);
 		
+		assertTrue(icon != null);
+
 	}
 }
