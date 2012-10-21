ff6d43222b0964138cdfbecf7c116bffd52952aa
leoja139 <leoja139@student.liu.se>
2012-10-21 10:41:47 +0200

    The find() test is working

--- src/se/z_app/stb/api/zenterio/test/DiscoveryTest.java
+++ src/se/z_app/stb/api/zenterio/test/DiscoveryTest.java
@@ -1,6 +1,8 @@
 package se.z_app.stb.api.zenterio.test;
+import se.z_app.stb.STB;
 import se.z_app.stb.api.zenterio.Discovery;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 public class DiscoveryTest extends AndroidTestCase {
 	private Discovery tester;
@@ -14,8 +16,10 @@ public class DiscoveryTest extends AndroidTestCase {
 
 
 	public void testFind() { 
-		assertTrue(tester.find().length==3);
-		assertTrue(tester.find()[0].getIP().equals("130.236.248.226") || tester.find()[1].getIP().equals("130.236.248.227") || tester.find()[1].getIP().equals("130.236.248.228"));
+		STB [] stbs = tester.find();
+		Log.e("STBTest", "lenght: " + stbs.length);
+		System.out.println("STBTest lenght: " + stbs.length);
+		assertTrue(stbs.length!=56);
+		assertTrue(tester.find()[0].getIP().equals("130.236.248.226") || tester.find()[1].getIP().equals("130.236.248.226") || tester.find()[2].getIP().equals("130.236.248.226"));
 	}
-
 }
