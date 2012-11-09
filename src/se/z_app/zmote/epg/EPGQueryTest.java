3e754f602394ec38737dd4b5a7cced0996dc502b
marwi509 <marwi509@student.liu.se>
2012-11-09 18:31:26 +0100

    Added a test case for the EPGQuery.searchProgram(Date, long)

--- src/se/z_app/zmote/epg/EPGQueryTest.java
+++ src/se/z_app/zmote/epg/EPGQueryTest.java
@@ -25,23 +25,31 @@ public class EPGQueryTest extends AndroidTestCase {
 	{
 		theQuery = new EPGQuery();
 		
-		Channel channel = new Channel(); //create a channel
-		channel.setName("BBC1");
-		channel.setNr(1);
-		channel.setIconUrl("/res/icon.png");
-		channel.setOnid(1);
-		channel.setSid(2);
-		channel.setTsid(3);
-
 		Channel channel1 = new Channel(); //create a channel
-		channel1.setName("SVT");
-		channel1.setNr(2);
+		channel1.setName("BBC1");
+		channel1.setNr(1);
 		channel1.setIconUrl("/res/icon.png");
-		channel1.setOnid(10);
-		channel1.setSid(11);
-		channel1.setTsid(12);
+		channel1.setOnid(1);
+		channel1.setSid(2);
+		channel1.setTsid(3);
 
-		Program program = new Program(channel);  //create a program
+		Channel channel2 = new Channel(); //create a channel
+		channel2.setName("SVT");
+		channel2.setNr(2);
+		channel2.setIconUrl("/res/icon.png");
+		channel2.setOnid(10);
+		channel2.setSid(11);
+		channel2.setTsid(12);
+		
+		Channel channel3 = new Channel(); //create a channel
+		channel3.setName("TV4");
+		channel3.setNr(3);
+		channel3.setIconUrl("/res/icon.png");
+		channel3.setOnid(21);
+		channel3.setSid(22);
+		channel3.setTsid(23);
+
+		Program program = new Program(channel1);  //create a program
 		program.setName(PROGRAM_NAME);
 		program.setEventID(1337);
 		@SuppressWarnings("deprecation")
@@ -61,7 +69,7 @@ public class EPGQueryTest extends AndroidTestCase {
 		program2.setShortText("How I met your mother, nice show");
 		program2.setLongText("Meet Ted and is infinite search for finding the one");
 
-		Program program3 = new Program(channel);
+		Program program3 = new Program(channel1);
 		program3.setName(PROGRAM3_NAME);
 		program3.setEventID(1339);
 		@SuppressWarnings("deprecation")
@@ -71,24 +79,51 @@ public class EPGQueryTest extends AndroidTestCase {
 		program3.setShortText("TopGear, start your engines");
 		program3.setLongText("The longrunner is back, featuring Clarkson, May and James, don's miss it");
 		
-		channel.addProgram(program);
-		channel.addProgram(program3);
-		channel1.addProgram(program2);
+		Program earlyProgram = new Program(channel1);
+		earlyProgram.setName("Early Program");
+		earlyProgram.setStart(new Date(System.currentTimeMillis() - 3600 * 1000));
 		
-		EPGContentHandler.instance().getEPG().addChannel(channel);
+		Program currentProgram = new Program(channel2);
+		currentProgram.setName("Current Program");
+		currentProgram.setStart(new Date(System.currentTimeMillis() + 1000 * 1000));
+		
+		Program lateProgram = new Program(channel3);
+		lateProgram.setName("Late Program");
+		lateProgram.setStart(new Date(System.currentTimeMillis() + 3600 * 2000));
+		
+		channel1.addProgram(program);
+		channel1.addProgram(program3);
+		channel1.addProgram(earlyProgram);
+		channel2.addProgram(program2);
+		channel2.addProgram(currentProgram);
+		channel3.addProgram(lateProgram);
+		
 		EPGContentHandler.instance().getEPG().addChannel(channel1);
+		EPGContentHandler.instance().getEPG().addChannel(channel2);
+		EPGContentHandler.instance().getEPG().addChannel(channel3);
 	}
 	
 	public void tearDown(){}
 	
 	/**
-	 * Test if the search query works
+	 * Test if the search query using string works
 	 */
-	public void testSearchProgram()
+	public void testStringSearchProgram()
 	{
 		assertTrue(theQuery.searchProgram(PROGRAM_NAME).length == 2);
 		assertTrue(theQuery.searchProgram(PROGRAM3_NAME).length == 1);
 		assertTrue(theQuery.searchProgram(PROGRAM2_NAME).length == 1);
 		assertTrue(theQuery.searchProgram("This program does not exist.").length == 0);
+	}
+	
+	/**
+	 * Test if the search query using time intervals works
+	 */
+	public void testIntervalSearchProgram()
+	{
+		assertTrue(theQuery.searchProgram(new Date(System.currentTimeMillis()), 1000 * 750).length == 0);
+		assertTrue(theQuery.searchProgram(new Date(System.currentTimeMillis()), 1000 * 1500).length == 1);
+		assertTrue(theQuery.searchProgram(new Date(System.currentTimeMillis()), 3600 * 1500).length == 2);
+		assertTrue(theQuery.searchProgram(new Date(System.currentTimeMillis()), 3600 * 2500).length == 3);
 	}
 }
