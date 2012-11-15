4130e7b59c176b3962a9712c9feaac2af2ee9004
steko682 <Stefan@130.236.209.243>
2012-11-15 14:43:58 +0100

    Adding comments and author

--- src/se/z_app/stb/api/zenterio/test/APIFactoryZenterioTestInt.java
+++ src/se/z_app/stb/api/zenterio/test/APIFactoryZenterioTestInt.java
@@ -5,28 +5,39 @@ import se.z_app.stb.api.*;
 import se.z_app.test_utils.STBFactory;
 import junit.framework.TestCase;
 
+/**
+ * 
+ * @author Viktor Håkansson, Stefan Konac
+ * 
+ */
+
 
 public class APIFactoryZenterioTestInt extends TestCase {
+// Creating an STB as the STB1
 	STB stb = STBFactory.get1();
 	APIFactoryZenterio factory = new APIFactoryZenterio(stb);
 	
 
 	public void testGetDiscovery() {
+		//Testing the factory procedure getDiscovery
 		Discovery discovery = (Discovery) factory.getDiscovery();
 		assertTrue(discovery instanceof Discovery);
 	}
 
 	public void testGetBiDirectional() {
+		//Testing the factory procedure getBiDirectional
 		StandardCommand standardCommand = (StandardCommand) factory.getBiDirectional();
 		assertTrue(standardCommand instanceof BiDirectionalCmdInterface);
 	}
 
 	public void testGetMonoDirectional() {
+		//Testing the factory procedure getMonoDirectional
 		RCCommand monoCommand = (RCCommand) factory.getMonoDirectional();
 		assertTrue(monoCommand instanceof MonoDirectionalCmdInterface);
 	}
 
 	public void testGetEventListner() {
+		//Testing the factory procedure getEventListener
 		EventListener listner = (EventListener) factory.getEventListner();
 		assertTrue(listner instanceof EventListnerInterface);
 	}
