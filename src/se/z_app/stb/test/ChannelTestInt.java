cc0528a1f3c1c6f83d3119574ca37038498880f9
steko682 <Stefan@130.236.209.243>
2012-11-17 10:51:08 +0100

    Integration test for WebTVService and WebTVItem (2.1.1).

---
+++ src/se/z_app/stb/test/ChannelTestInt.java
@@ -0,0 +1,19 @@
+package se.z_app.stb.test;
+
+import junit.framework.TestCase;
+
+public class ChannelTestInt extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testAddProgram() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/se/z_app/stb/test/WebTVItemTestInt.java
+++ src/se/z_app/stb/test/WebTVItemTestInt.java
@@ -5,6 +5,12 @@ import se.z_app.stb.WebTVService;
 import android.graphics.Bitmap;
 import android.test.AndroidTestCase;
 
+/**
+ * 
+ * @author Emanuel Reneby, Viktor von Zeipel and Stefan Konac
+ * ID 2.1.1
+ * Integration test for the classes WebTVService and WebTVItem.
+ */
 public class WebTVItemTestInt extends AndroidTestCase {
 
 	private static final String author = "authorString";
@@ -18,10 +24,12 @@ public class WebTVItemTestInt extends AndroidTestCase {
 	private static final String iconURLS = "iconURLS";
 	private static final String idS = "idS";
 	private static final String name = "name";
+	private WebTVItem wti;
+	private WebTVService wts;
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		WebTVItem wti = new WebTVItem();
+		wti = new WebTVItem();
 		wti.setAuthor(author);
 		wti.setDuration(duration);
 		wti.setIcon(iconI);
@@ -29,18 +37,24 @@ public class WebTVItemTestInt extends AndroidTestCase {
 		wti.setId(idI);
 		wti.setInfo(info);
 		wti.setTitle(title);
-		WebTVService wts = new WebTVService();
+		wts = new WebTVService();
 		wts.setIcon(iconS);
 		wts.setIconURL(iconURLS);
 		wts.setID(idS);
 		wts.setName(name);
 	}
 	
-	
-	
-
 	protected void tearDown() throws Exception {
 		super.tearDown();
+	}
+	
+	public void testWebTVItemTestInt() {
+		wti.setWebTVService(wts);
+		assertEquals(wts, wti.getWebTVService());
+		assertEquals(wts.getIconURL(), wti.getWebTVService().getIconURL());
+		assertEquals(wts.getID(), wti.getWebTVService().getID());
+		assertEquals(wts.getName(), wti.getWebTVService().getName());
+		assertEquals(wts.getIcon(), wti.getWebTVService().getIcon());
 	}
 
 }
