0c2a99849b8fede22393aeec51c9f3a5ed982baf
steko682 <Stefan@130.236.209.243>
2012-11-17 16:00:53 +0100

    Created DefaultStateTestInt. Complete.

---
+++ src/se/z_app/stb/api/test/DefaultStateTestInt.java
@@ -0,0 +1,69 @@
+package se.z_app.stb.api.test;
+
+import java.io.File;
+
+import se.z_app.httpserver.ZmoteHTTPD;
+import se.z_app.stb.STB;
+import se.z_app.stb.api.RCProxy;
+import se.z_app.stb.api.RCProxyState;
+import se.z_app.stb.api.STBContainer;
+import se.z_app.test_utils.HTTPRequestHandlerTestContainer;
+import se.z_app.test_utils.STBFactory;
+import android.test.AndroidTestCase;
+
+public class DefaultStateTestInt extends AndroidTestCase {
+	
+	private static final String uri = "/cgi-bin/writepipe_key";
+	private static final long sleepTime = 200;
+	private ZmoteHTTPD httpd;
+	private HTTPRequestHandlerTestContainer container;
+	private STB stb;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		int port = 3000 + (int)(Math.random()*7000);
+		httpd = new ZmoteHTTPD(port, new File(".").getAbsoluteFile());
+		stb = STBFactory.getLocal(port);
+		STBContainer.instance().setActiveSTB(stb);
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testDefaultStateTestInt() {
+		container = new HTTPRequestHandlerTestContainer(uri);
+		httpd.addHandler(container);
+		RCProxyState ds = RCProxy.instance().getState();
+		ds.up();
+		assertTrue(check().equals("pup"));
+		ds.down();
+		assertTrue(check().equals("pdown"));
+		ds.right();
+		assertTrue(check().equals("pright"));
+		ds.left();
+		assertTrue(check().equals("pleft"));
+		ds.ok();
+		assertTrue(check().equals("pok"));		
+		ds.back();	
+		assertTrue(check().equals("pback"));
+		ds.mute();
+		assertTrue(check().equals("pmute"));
+		ds.info();
+		assertTrue(check().equals("pinfo"));
+		ds.menu();
+		assertTrue(check().equals("pmenu"));
+		ds.exit();
+		assertTrue(check().equals("pexit"));
+	}
+	
+	private String check() {
+		try {
+			Thread.sleep(sleepTime);
+		} catch (InterruptedException e) {}
+		String returnedPost = (String)container.getParms().get("raw");
+		assertTrue(container.getMethod().equals("POST"));
+		return returnedPost;
+	}
+	
+}
